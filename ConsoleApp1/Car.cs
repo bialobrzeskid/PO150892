802c6706eb3bba2ba264f7d07615a46a5c4ff0d0
Daniel Białobrzeski <bialobrzeski.daniel@gmail.com>
2019-10-23 14:39:05 +0200

    zmiana 2

---
+++ ConsoleApp1/Car.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp1
+{
+    class Car
+    {
+        string model;
+
+        public Car(string model)
+        {
+            this.model = model;
+        
+        }
+    }
+}

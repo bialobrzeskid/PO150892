f0cae3e9748c649a6b83851d106493981a22eff5
BIALY <46341840+bialobrzeskid@users.noreply.github.com>
2019-10-30 15:49:37 +0100

    Add files via upload

---
+++ Wyjatki-3zajecia/Wyjatki-3zajecia/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wyjatki_3zajecia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int liczba1, liczba2, liczba3;
+            liczba2 = 30;
+            liczba3 = 0;
+
+
+
+
+            int[] table = new int[2];
+            try
+            {
+                throw new IndexOutOfRangeException();
+            }
+            catch
+            {
+                Console.WriteLine("Cos");
+            }
+            
+
+
+
+
+
+
+        }   } 
+
+} 

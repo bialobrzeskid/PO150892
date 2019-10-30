ef6f088402ba1e93589b576f660ca22763b3b307
BIALY <46341840+bialobrzeskid@users.noreply.github.com>
2019-10-30 15:24:26 +0100

    Add files via upload

---
+++ Cwiczenia3-git/Cwiczenia3-git/Car.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows.Forms;
+
+namespace Cwiczenia3_git
+{
+    class Car
+    {
+        private double pojemnoscSilnika;
+        private string marka;
+        static int iloscKol;
+
+        private Car()
+        {
+
+        }
+
+        private Car(double pojemnosc, string marka)
+        {
+            this.pojemnoscSilnika = 2;
+            this.marka = "Jakas marka ";
+
+        }
+
+        static Car() //statyczny konstruktor 
+        {
+            iloscKol = 4;
+        }
+
+        public static Car Create()
+        {
+            Car obCar = new Car();
+            return obCar;
+        }
+
+        ~Car()
+        {
+            MessageBox.Show("zwalniam pamiec");
+        }
+    }
+}
---
+++ Cwiczenia3-git/Cwiczenia3-git/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cwiczenia3_git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car.Create();
+
+
+        }
+    }
+}

5839ceed61777788659787053b24cd1efe402f32
BIALY <46341840+bialobrzeskid@users.noreply.github.com>
2019-10-30 16:10:57 +0100

    Add files via upload

---
+++ dodawanie-zajecia3/dodawanie-zajecia3/Obliczenia.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace dodawanie_zajecia3
+{
+    class Obliczenia
+    {
+        public double wynik;
+        public double a;
+        public double b;
+
+
+        public double Dodawanie (double a, double b)
+        {
+            wynik = a + b;
+            return wynik;
+        }
+        public double Odejmowanie (double a, double b)
+        {
+            wynik = a - b;
+            return wynik;
+        }
+        public double Mnozenie(double a, double b)
+        {
+            wynik = a * b;
+            return wynik;
+        }
+        public double Dzielenie(double a, double b)
+        {
+            try
+            {
+                wynik = a / b;
+               
+            }
+            catch
+            {
+                Console.WriteLine("Nie mozna dzielic przez zero");
+            }
+            return wynik;
+        }
+        public double Potegowanie(double a, double b)
+        {
+
+            return Math.Pow(a, b);
+        }
+        public double Pierwiastkowanie(double a)
+        {
+            wynik = Math.Sqrt(a);
+            return wynik;
+        }
+    }
+}
---
+++ dodawanie-zajecia3/dodawanie-zajecia3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dodawanie_zajecia3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Obliczenia kalklulator = new Obliczenia();
+            kalklulator.a = 5;
+            kalklulator.b = 4;
+
+            Console.WriteLine(kalklulator.Dodawanie(kalklulator.a, kalklulator.b));
+            Console.WriteLine(kalklulator.Potegowanie(kalklulator.a, kalklulator.b));
+        }
+
+
+    }
+}
